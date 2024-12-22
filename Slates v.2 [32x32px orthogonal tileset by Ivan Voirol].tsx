2a706519a9b70dbaab727f8fddf717d7c749791f
Shionn (Kepka Ludovic) <shionn@gmail.com>
2024-12-22 23:55:53 +0100

    init

---
+++ Slates v.2 [32x32px orthogonal tileset by Ivan Voirol].tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Slates v.2 [32x32px orthogonal tileset by Ivan Voirol]" tilewidth="32" tileheight="32" tilecount="1288" columns="56">
+ <image source="Slates v.2 [32x32px orthogonal tileset by Ivan Voirol].png" width="1792" height="736"/>
+</tileset>
