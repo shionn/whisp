59bab873390ed14ff4b2051b23598dda4a7ac290
Shionn (Kepka Ludovic) <shionn@gmail.com>
2024-12-30 18:47:31 +0100

    un peu de carte

---
+++ lpc-ship.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="lpc-ship" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="lpc-ship/lpc-ship/lpc-ship.png" width="1024" height="1024"/>
+</tileset>
---
+++ new tiles 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="new tiles 1" tilewidth="32" tileheight="32" tilecount="42" columns="7">
+ <image source="new tiles 1.png" width="250" height="220"/>
+</tileset>
