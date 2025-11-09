acc3b53438580a42564db8fca2bd09c2ae9e6644
Kepka Ludovic <shionn@gmail.com>
2025-11-09 15:15:28 +0100

    nouvelle carte

---
+++ Tiny-Islands-by-Majadroid.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Tiny-Islands-by-Majadroid" tilewidth="32" tileheight="32" tilecount="35" columns="7">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="tiny-islands-by-majadroid/Tiny-Islands-by-Majadroid/tilemap.png" width="224" height="160"/>
+</tileset>
---
+++ tiny-island.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiny-island" tilewidth="16" tileheight="16" tilecount="140" columns="14">
+ <image source="tiny-islands-by-majadroid/Tiny-Islands-by-Majadroid/tilemap.png" width="224" height="160"/>
+</tileset>
